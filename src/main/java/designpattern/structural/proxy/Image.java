686059d5e75f6686ee16d19b5fb1fcb050c6525f
Pablo <p5f8@hotmail.com>
2016-11-10 20:36:13 +0000

    Add Proxy design pattern

---
+++ src/main/java/designpattern/structural/proxy/Image.java
@@ -0,0 +1,7 @@
+package designpattern.structural.proxy;
+
+public interface Image {
+	
+	void display();
+
+}
---
+++ src/main/java/designpattern/structural/proxy/ProxyImage.java
@@ -0,0 +1,21 @@
+package designpattern.structural.proxy;
+
+public class ProxyImage implements Image {
+	
+	private RealImage realImage;
+	private String fileName;
+	
+	public ProxyImage(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public void display() {
+		if(realImage == null) {
+			realImage = new RealImage(this.fileName);
+		}
+		realImage.display();
+	}
+	
+	
+
+}
