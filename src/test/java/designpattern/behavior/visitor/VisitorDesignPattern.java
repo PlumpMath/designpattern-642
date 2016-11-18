04d3341ddc475b6ad2d85d5bf4b30de109cc3de8
Pablo <p5f8@hotmail.com>
2016-11-18 17:13:46 +0000

    Corrections tests visitor design pattern

--- src/test/java/designpattern/behavior/visitor/VisitorDesignPattern.java
+++ src/test/java/designpattern/behavior/visitor/VisitorDesignPattern.java
@@ -1,13 +1,41 @@
 package designpattern.behavior.visitor;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class VisitorDesignPattern {
+	
+	private PrintStream old = System.out;
+	private ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	private PrintStream ps = new PrintStream(baos);
+	
+	@Before
+	public void beforeTests() {
+	    System.setOut(ps);
+	}
+	
+	@After
+	public void afterTest() {
+		System.setOut(old);
+	}	
 	
 	@Test
 	public void testVisitorPattern() {
 		ComputerPart computer = new Computer();
 		computer.accept(new ComputerPartDisplayVisitor());
+		
+		String message = "Displaying Mouse.\r\n" +
+							"Displaying Keyboard.\r\n" +
+							"Displaying Monitor.\r\n" +
+							"Displaying Computer.\r\n";
+		
+		assertEquals(message, baos.toString());
 	}
 	
 }
